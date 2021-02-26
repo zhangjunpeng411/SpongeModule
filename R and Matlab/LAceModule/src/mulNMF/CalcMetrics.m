352b9a7f2fbb22f73ba892d9b2e06753f20f0d41
Junpeng Zhang <zhangjunpeng_411@yahoo.com>
2021-02-26 20:19:19 +0800

    Delete R and Matlab directory

--- R and Matlab/LAceModule/src/mulNMF/CalcMetrics.m
+++
@@ -1,8 +0,0 @@
-function [AC, nmi_value, error_cnt] = CalcMetrics(label, result)
-
-result = bestMap(label, result);
-error_cnt = sum(label ~= result);
-AC = length(find(label == result))/length(label);
-
-nmi_value = nmi(label, result);
-
